c168b087262ac43eb41d6c851b6eda9776b2272c
sirmos <sirmos34@yahoo.com>
2023-11-21 19:01:21 -0800

    Simple shell

--- history.c
+++ history.c
@@ -1,15 +1,15 @@
 #include "shell.h"
 
 /**
- *  * get_history_file - gets the history file
- *   * @info: parameter struct
- *    *
- *     * Return: allocated string containg history file
- *      */
+ * get_history_file - gets the history file
+ * @info: parameter struct
+ *
+ * Return: allocated string containg history file
+ */
 
 char *get_history_file(info_t *info)
 {
-		char *buf, *dir;
+	char *buf, *dir;
 
 			dir = _getenv(info, "HOME=");
 				if (!dir)
